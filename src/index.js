901fb710396e357b0f02ffd17d9fb7e7be91a74f
iOnline247 <remymaza@gmail.com>
2020-07-25 18:58:27 -0400

    Added ::logging:: event.

--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@ import { v4 as uuidv4 } from "uuid";
 import Logger from "./logger.js";
 
 const logger = new Logger({
-    appName: "nodejs-boilerplate",
-    component: "Typescript",
-    module: "DX",
-    correlationId: uuidv4(),
-    jobId: uuidv4()
+  appName: "nodejs-boilerplate",
+  component: "Typescript",
+  module: "DX",
+  correlationId: uuidv4(),
+  jobId: uuidv4(),
+});
+
+logger.on(Logger.EVENTS.LOGGING, async (payload) => {
+  console.log(`Logger event fired: ${Logger.EVENTS.LOGGING}`);
+  console.log(payload);
 });
 
 logger.on(Logger.EVENTS.LOGGED, async (payload) => {
-    console.log(`Logger event fired: ${Logger.EVENTS.LOGGED}`);
-    console.log(payload);
-
-    debugger;
+  console.log(`Logger event fired: ${Logger.EVENTS.LOGGED}`);
+  console.log(payload);
 });
 
 logger.log("Hola, Mundo");
